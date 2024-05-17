01d5f662ab777471e24d6eadf8b040a3bd458801
Akshaykulthe18 <101685162+Akshaykulthe18@users.noreply.github.com>
2024-05-17 17:20:04 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 # Service Principal Variables
 
 variable "client_id" {
-    description =   "c2f95825-00de-4569-83ce-060d20505ccb"
+    description =   "1e79ea61-0414-4937-9ab8-92f97632f1ca"
     type        =   string
 }
 
 variable "client_secret" {
-    description =   ".qV8Q~yxs..AMWV5SjdDjYrF3d3h2eofH3C1xdj8"
+    description =   "PKf8Q~ZBsiKVSE1v91gCLL_LSwCCF6h3.uNsFaYg"
     type        =   string
 }
 
 variable "subscription_id" {
-    description =   "126115f9-75e7-4789-9da4-ed851551fcd6"
+    description =   "2c232cc6-1a70-4669-bd20-3ade791c35ce"
     type        =   string
 }
 
 variable "tenant_id" {
-    description =   "e4f8a9c5-0fb7-4f4d-b13a-a4c2abae4bf7"
+    description =   "c48e4112-9a8f-418b-a115-e5929b0c550a"
     type        =   string
 }
